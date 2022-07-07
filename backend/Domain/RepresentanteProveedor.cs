2a2a6317243a2bbdda4ce2dde9722ea607b229d6
Giovani Meza <giovanidav5@outlook.com>
2022-07-07 16:58:04 -0600

    Patches entities partially
    Entity relationship model must be updated

---
+++ backend/Domain/RepresentanteProveedor.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class RepresentanteProveedor
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+        
+    }
+}
